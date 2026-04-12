a55926a0cade5887cfff2418fc8762f0286621f3
Raghad-S12 <Raghadr2424@gmail.com>
2026-04-12 16:04:14 +0000

    Initial commit

---
+++ Course3/HW4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"M1\\M2\n";
+    cout<<"M3\tM4\n";
+    cout<<"My name is \" Raghad \" \n";
+    cout<<"\a \n";
+
+   
+    cout<<"Dear Sir\\Madam,\n\nHow are you?\n\nMy name is \"Mohammed\" ,nice to meet you\n`";
+
+
+    return 0;
+ 
+}
